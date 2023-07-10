7ef6d06a8392b15847f651fd36fbf41e85f750c3
6xiaozhou <89899123+6xiaozhou@users.noreply.github.com>
2023-07-10 17:44:50 +0800

    first commit

---
+++ demo.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("boston_house_prices.csv", os.O_RDONLY, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file) //具有解码功能，能够将csv文件解码成二维数组
+	//fmt.Println(string(data))
+	data, _ := reader.ReadAll()
+	//fmt.Println(len(data))
+	//fmt.Println(len(data[0]))
+	//划分训练集以及测试集，划分比例为8:2
+	//trainingDataX := data[1:405][:14]
+	//trainingDataY := data[:][14:]
+	//testDataX := data[405:][:14]
+	//fmt.Println(trainingDataX)
+	fmt.Println("---------------")
+	fmt.Println(len(data), len(data[0]))
+	s := data[:405]
+	//fmt.Println(len(testDataX))
+	fmt.Println(s[0][0])
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoLinerRegression
+
+go 1.20
